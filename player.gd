25f796c9025e050a1b1f56b9ff57736d3d33da1f
Robert Chłopek <robuschlopek@gmail.com>
2023-10-02 19:45:01 +0200

    Drobne zmiany w systemie poruszania.

--- player.gd
+++ player.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 @export var speed = 40.0
+@export var acceleration = 1.0
+
 @export var jump_height = 100.0
 @export var jump_descent = 0.5
 @export var jump_peak = 0.7
 
-# Wartości prędkości na podstawie równania rzutu pionowego
-var jump_speed = ((2.0 * jump_height) / jump_peak) * -1
-var jump_gravity = ((-2.0 * jump_height) / (jump_peak * jump_peak)) * -1
-var fall_gravity = ((-2.0 * jump_height) / (jump_descent * jump_descent)) * -1
+var jump_speed
+var jump_gravity
+var fall_gravity
+
+var dir = 0.0
 
 # | ============================================================================= |
 
@@ -16,6 +19,7 @@ var fall_gravity = ((-2.0 * jump_height) / (jump_descent * jump_descent)) * -1
 func _ready():
 	print("hello world")
 	
+	# Wartości prędkości podczas skoku na podstawie równania rzutu pionowego
 	jump_speed = ((2.0 * jump_height) / jump_peak) * -1
 	jump_gravity = ((-2.0 * jump_height) / (jump_peak * jump_peak)) * -1
 	fall_gravity = ((-2.0 * jump_height) / (jump_descent * jump_descent)) * -1
@@ -27,12 +31,12 @@ func _physics_process(delta):
 	velocity.y += get_gravity() * delta
 	
 	# Poruszanie się na boki
-	var input_dir = 0.0
 	if Input.is_action_pressed("move_right"):
-		input_dir += 1.0
+		set_direction("right")
 	if Input.is_action_pressed("move_left"):
-		input_dir += -1.0
-	velocity.x = input_dir * speed
+		set_direction("left")
+		
+	velocity.x = dir * min(acceleration + abs(velocity.x), speed)
 	
 	# Pobranie eventu jump (domyślnie spacja i strzałka w górę) oraz skok
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -49,6 +53,16 @@ func get_gravity() -> float:
 		return fall_gravity
 	else:
 		return jump_gravity
+
+# Zmienia kierunek poruszania się postaci
+func set_direction(direction):
+	match direction:
+		"right":
+			dir = 1.0
+			scale.x = scale.y * 1
+		"left":
+			dir = -1.0
+			scale.x = scale.y * -1
 
 # Skok
 func jump():

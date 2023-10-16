af50545b12c9ba9014c3f8ac6789bd26b62de1de
Robert Chłopek <robuschlopek@gmail.com>
2023-10-16 18:43:34 +0200

    System życia gracza oraz wroga, otrzymywanie obrażeń przez wroga

--- enemy.gd
+++ enemy.gd
@@ -6,19 +6,33 @@ extends CharacterBody2D
 const GRAVITY = 200.0
 var dir = 0.0
 
+var health : float
+@export var MAX_HEALTH = 100.0
+
+@export var iframes = 0.5
+
+@onready var sprite = get_node("Enemy")
+@onready var dir_timer = get_node("dir_timer")
+@onready var player = get_node("../player")
+
 
 # | ============================================================================= |
 
 
 func _ready():
-	var dir_timer = get_node("dir_timer")
 	dir_timer.start()
+	$healthbar.hide()
+	health = MAX_HEALTH
 	
 	# Timer wysyła sygnał o zakończeniu liczenia, przez co aktywuje get_dir()
 	dir_timer.timeout.connect(get_dir)
+	$health_timer.timeout.connect(hide_health)
 
 
 # | ============================================================================= |
+
+func _process(delta):
+	$healthbar.value = health
 
 
 # Obsługuje fizykę
@@ -27,7 +41,12 @@ func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 	
 	# Nadanie prędkości poziomej
-	velocity.x = dir * min(acceleration + abs(velocity.x), speed)
+	if is_on_floor():
+		velocity.x = dir * min(acceleration + abs(velocity.x), speed)
+	
+	# Test ataku
+	if Input.is_action_pressed("jump"):
+		apply_damage(10, 25, player.position)
 	
 	# Obsługuje poruszanie i kolizję
 	move_and_slide()
@@ -44,12 +63,34 @@ func get_dir():
 	# Zmienia kierunek oraz odwraca teksturę wroga
 	if chance < 0.3:
 		dir = 1.0
-		scale.x = scale.y * 1
+		sprite.scale.x = sprite.scale.y * 1
 	elif chance < 0.6:
 		dir = -1.0
-		scale.x = scale.y * -1
+		sprite.scale.x = sprite.scale.y * -1
 	else:
 		dir = 0
+
+
+# Zadaje obrażenia oraz pokazuje pasek życia
+func apply_damage(damage, knockback, pos : Vector2):
+	if $damage_timer.is_stopped():
+		health -= damage
+		apply_knockback(knockback, pos)
+		$healthbar.show()
+		$health_timer.start()
+		$damage_timer.start()
+
+
+# Odrzucenie podczas otrzymywania obrażeń
+func apply_knockback(strength, pos : Vector2):
+	var direction = pos.direction_to(position) + Vector2(0, -3)
+	velocity = direction * strength
+
+
+# Ukrywa pasek życia po określonym czasie
+func hide_health():
+	$healthbar.hide()
+	$health_timer.stop()
 
 
 # | ============================================================================= |
--- player.gd
+++ player.gd
@@ -9,18 +9,19 @@ extends CharacterBody2D
 @export var jump_descent = 0.5
 @export var jump_peak = 0.7
 
-var jump_speed
-var jump_gravity
-var fall_gravity
-
 # Kierunek poruszania się
 var dir = 0.0
 
 var health : float
 @export var MAX_HEALTH = 100.0
 
+# Wartości prędkości podczas skoku na podstawie równania rzutu pionowego
+@onready var jump_speed = ((2.0 * jump_height) / jump_peak) * -1
+@onready var jump_gravity = ((-2.0 * jump_height) / (jump_peak * jump_peak)) * -1
+@onready var fall_gravity = ((-2.0 * jump_height) / (jump_descent * jump_descent)) * -1
+
 @onready var sprite = get_node("AnimatedSprite2D")
-@onready var healthbar = get_node("CanvasLayer/healthbar")
+@onready var healthbar = get_node("HUD/healthbar")
 
 # | ============================================================================= |
 
@@ -29,11 +30,6 @@ var health : float
 func _ready():
 	disable_player()
 	hide()
-	
-	# Wartości prędkości podczas skoku na podstawie równania rzutu pionowego
-	jump_speed = ((2.0 * jump_height) / jump_peak) * -1
-	jump_gravity = ((-2.0 * jump_height) / (jump_peak * jump_peak)) * -1
-	fall_gravity = ((-2.0 * jump_height) / (jump_descent * jump_descent)) * -1
 
 # Wywoływane podczas każdej klatki
 func _process(delta):
@@ -44,14 +40,12 @@ func _physics_process(delta):
 	# Dodanie grawitacji
 	velocity.y += get_gravity() * delta
 	
-	dir = 0
-	
 	# Poruszanie się na boki
+	dir = 0
 	if Input.is_action_pressed("move_right"):
 		set_direction("right")
 	if Input.is_action_pressed("move_left"):
 		set_direction("left")
-
 
 	# Nadaje poziomą prędkość postaci
 	velocity.x = dir * min(acceleration + abs(velocity.x), speed)

e4b62aed100111a702cf7f0614a1c52eab9740b3
Robert Chłopek <robuschlopek@gmail.com>
2023-12-08 14:11:16 +0100

    Demonstracyjna tekstura finalnej postaci gracza

--- assets/test_level/test_level.gd
+++ assets/test_level/test_level.gd
@@ -6,17 +6,15 @@ extends Level
 
 @onready var clouds = get_node("ParallaxBackground/clouds_layer/clouds")
 
+
 # | ============================================================================= |
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
-	
-#func menu():
-#	if Input.is_action_just_pressed("escape"):
-#		get_tree().change_scene_to_file("res://assets/main_menu/main_menu.tscn")
-		
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	super(delta)

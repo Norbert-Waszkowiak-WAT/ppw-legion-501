785b2e0bfaa2102b0f5e7c3145bfa41b7fb3a8b2
Robert Chłopek <robuschlopek@gmail.com>
2023-12-20 13:13:24 +0100

    Poprawki w drzewku rozwoju

--- assets/ui/skill_tree/skill_button.gd
+++ assets/ui/skill_tree/skill_button.gd
@@ -2,19 +2,29 @@ extends TextureButton
 
 
 @export var previous_button : Node
+@export var skill_name : String
 
 @onready var line = $Line2D
+@onready var popup = $PopupPanel
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	if previous_button:
-		print(str(previous_button.global_position) + " => " + str(global_position))
-		line.add_point(position + size/2)
-		line.add_point(previous_button.position + previous_button.size/2)
+	popup.title = skill_name
+	pivot_offset = size/2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	popup.position = get_global_mouse_position()
+	if is_hovered():
+		popup.show()
+	else:
+		popup.hide()
+	print(is_hovered())
+	
+	line.clear_points()
+	if previous_button:
+		#print(str(previous_button.global_position) + " => " + str(global_position))
+		line.add_point(global_position + size/2)
+		line.add_point(previous_button.global_position + size/2)
